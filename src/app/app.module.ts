41f42bf504f48178b3314cd30ef12f904ce32b09
Isrrael Gutierrez <isrrael.gutierrez@definityfirst.com>
2023-04-13 15:00:04 -0500

    adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { UsersListComponent } from './users-list/users-list.component';
@@ -10,7 +11,8 @@ import { UsersListComponent } from './users-list/users-list.component';
     UsersListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
