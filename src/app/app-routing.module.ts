117a8c375167bb92a34701f24969247dfde45824
Isrrael Gutierrez <isrrael.gutierrez@yahoo.com.mx>
2023-04-13 15:12:00 -0500

    adding user component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersListComponent } from './users-list/users-list.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-  { path: '', component: UsersListComponent },
+  { path: '', component: UsersListComponent},
+  { path: ':id', component: UserComponent}
+
 ];
 
 @NgModule({
