f474f847a9851a21fe7932a5b70c4fca0e22c926
Isrrael Gutierrez <isrrael.gutierrez@yahoo.com.mx>
2023-04-13 15:59:13 -0500

    Success !!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { UsersListComponent } from './users-list/users-list.component';
 import { CommonModule } from '@angular/common';
@@ -14,8 +14,9 @@ import { CommonModule } from '@angular/common';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonModule
-  ],
+    CommonModule,
+    BrowserAnimationsModule,
+ ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../services/user.service';
+import { trigger, style, animate, transition } from '@angular/animations';
+import * as marked from 'marked';
 
 @Component({
   selector: 'app-user',
   templateUrl: 'user.component.html',
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(1000, style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate(1000, style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class UserComponent implements OnInit {
   id: any;
@@ -20,11 +34,12 @@ export class UserComponent implements OnInit {
       this.id = +params['id'];
       this._userService.findOneById(this.id).subscribe(
         (data) => {
+          const _marked: any = marked;
           this.userData = data;
-          console.log(this.userData);
+          this.userData.bio = _marked.parse(data.bio);
         },
         (error) => {
-          this._router.navigate(['/'])
+          this._router.navigate(['/']);
         }
       );
     });
